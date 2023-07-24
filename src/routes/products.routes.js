d1dceeeeadebc634c5c24441cce9a72f3a306985
Alejandro Vacaro <avacaro@outlook.com>
2023-07-24 11:32:37 -0300

    Se solucionan errores en products.routes.js

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -26,23 +26,71 @@ router.get('/', async (req, res) => {
 
 // Ruta para obtener un producto específico por ID
 router.get('/:pid', async (req, res) => {
-    //...
+    try {
+        const id = parseInt(req.params.pid);
+        const product = await productManager.getProductById(id);
+
+        if (!product) {
+            return res.send({ error: 'Product does not exist' });
+        }
+
+        res.send(product);
+    } catch (error) {
+        res.send({ error: error.message });
+    }
 });
 
 // Ruta para añadir un nuevo producto
 router.post('/', async (req, res) => {
-    //...
+    try {
+        const { title, description, price, code, stock, category, thumbnails } = req.body;
+        const status = (req.body.status === 'true');
+        const isAdded = await productManager.addProduct(title, description, price, code, stock, category, thumbnails, status);
+
+        if (isAdded) {
+            res.send({ status: 'success', message: 'Product added successfully' });
+            const updatedProducts = await productManager.getProducts();
+            socketServer.emit('productAdded', updatedProducts);
+        } else {
+            res.status(400).send({ status: 'error', message: 'Failed to add product, code is repeated' });
+        }
+    } catch (error) {
+        res.status(400).send({ status: 'error', error: 'Invalid request' });
+    }
 });
 
 // Ruta para actualizar un producto específico
 router.put('/:pid', async (req, res) => {
-    //...
+    try {
+
+        const id = parseInt(req.params.pid);
+        const productUpdates = req.body;
+
+        await productManager.updateProduct(id, productUpdates);
+        res.send({ status: 'success', message: 'Product updated successfully' })
+
+    } catch (error) {
+        res.status(400).send({ status: 'error', error: 'Invalid request' });
+    }
 });
 
 // Ruta para eliminar un producto específico
 router.delete('/:pid', async (req, res) => {
-    //...
-});
+    try {
+
+        const id = parseInt(req.params.pid);
+        const product = await productManager.getProductById(id);
+        if (!product) {
+            return res.status(404).send({ error: 'Product does not exist' });
+        }
+        await productManager.deleteProduct(id);
+        const updatedProducts = await productManager.getProducts();
+        socketServer.emit('productDeleted', { status: 'success', message: 'Product deleted successfully', updatedProducts });
+        res.send({ status: 'success', message: 'Product deleted successfully' })
+    } catch (error) {
+        res.status(400).send({ status: 'error', error: 'Invalid request' });
+    }
+})
 
 // Exportamos el enrutador para usarlo en otros módulos
 export default router;
